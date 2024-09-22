d1e7320ef916478eb540781314a3d750e2f010c2
ernst250 <ernstsaincyr@gmail.com>
2024-09-22 14:17:37 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+const listMot= ["Chocolat","Banana","Cassave","Mangue","Avocat","Riz","Haricot","Epina","Epicerie","Boulangerie","Porcherie"]
+let score=0
+    let motUtilisateur= prompt("Entrer le mot:" +listMot[0]) 
+    if(motUtilisateur===listMot[0]){
+    score++
+}
+
+motUtilisateur= prompt("Entrer le mot:" +listMot[1])
+if(motUtilisateur===listMot[1]){
+    score++
+}
+
+motUtilisateur= prompt("Entrer le mot:" +listMot[2])
+if(motUtilisateur===listMot[2]){
+    score++
+
+}
+
+motUtilisateur= prompt("Entrer le mot:" +listMot[3])
+if(motUtilisateur===listMot[3]){
+    score++
+
+}
+
+motUtilisateur= prompt("Entrer le mot:" +listMot[4])
+if(motUtilisateur===listMot[4]){
+    score++
+
+}
+motUtilisateur= prompt("Entrer le mot:" +listMot[5])
+if(motUtilisateur===listMot[5]){
+    score++
+
+}
+motUtilisateur= prompt("Entrer le mot:" +listMot[6])
+if(motUtilisateur===listMot[6]){
+    score++
+
+}
+motUtilisateur= prompt("Entrer le mot:" +listMot[7])
+if(motUtilisateur===listMot[7]){
+    score++
+
+}motUtilisateur= prompt("Entrer le mot:" +listMot[8])
+if(motUtilisateur===listMot[8]){
+    score++
+
+}
+motUtilisateur= prompt("Entrer le mot:" +listMot[9])
+if(motUtilisateur===listMot[9]){
+    score++
+
+}
+
+
+
+console.log(score)
